585a8fa4fa4bb33a66e0c1707ae72813e3a7f36a
Philip Ingram <philip.d.ingram@gmail.com>
2014-05-10 01:07:47 -0700

    asteroids span at random spots and can have neg vel

--- asteroid.js
+++ asteroid.js
@@ -8,7 +8,7 @@
 
   Asteroid.RADIUS = 25;
   Asteroid.COLOR = '#888888';
-  Asteroid.MAXVEL = 8;
+  Asteroid.MAXVEL = 7;
 
   Asteroid.inherits(Asteroids.MovingObject);
 
@@ -24,13 +24,37 @@
       pos[whichIndex] = Asteroids.Game.DIM_X;
     }
     var vel = randomVec(Asteroid.MAXVEL);
+    pos = Asteroid.randomPos();
     return (new Asteroid(pos, vel));
+  }
+
+  Asteroid.randomPos = function() {
+    var pos = []
+    pos[0] = Math.floor(Math.random() * Asteroids.Game.DIM_X);
+    pos[1] = Math.floor(Math.random() * Asteroids.Game.DIM_Y);
+    randn = Math.floor(Math.random() * 4);
+    switch (randn) {
+      case 0:
+        pos[0] = 0;
+        break;
+      case 1:
+        pos[1] = 0;
+        break;
+      case 2:
+        pos[0] = Asteroids.Game.DIM_X;
+        break;
+      case 3:
+        pos[1] = Asteroids.Game.DIM_Y;
+        break;
+    }
+    return pos;
   }
 
   function randomVec(maxVel){
     var vec = [];
-    vec[0] = Math.floor(Math.random() * (maxVel - 2)) + 2;
-    vec[1] = Math.floor(Math.random() * (maxVel - 2)) + 2;
+    vec[0] = Math.floor(Math.random() * (maxVel * 2)) - maxVel;//- 2)) + 2;
+    vec[1] = Math.floor(Math.random() * (maxVel * 2)) - maxVel;// + 2;
+    // XXX- update velocity so that its abs val will not be 0 or 1
     return vec;
   }
 })(this);
